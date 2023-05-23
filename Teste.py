6f990d4e8651c9e99298215acc518c9b4944f4a2
Bernardo Santinoni <133998636+Besanti@users.noreply.github.com>
2023-05-23 08:00:49 -0300

    Create Teste.py

---
+++ Teste.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Teste ECMI 2')
+
+st.write('Tabela')
+
+dataframe = pd.DataFrame9{
+  'Nome': ['Josir', 'Bruno', 'Bruna', 'Anna']
+  'Salário: [10, 20, 30, 40]
+  ])
+  dataframe.style.highlight_max(axis=0)
+  
+  st.write(dataframe)
+  
