1371ccff56df664768d856f56a6f3f81d55297c8
Bernardo Santinoni <133998636+Besanti@users.noreply.github.com>
2023-05-23 08:08:19 -0300

    Update Teste.py
    copiei o codigo do josir

--- Teste.py
+++ Teste.py
@@ -1,15 +1,21 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.title('Teste ECMI 2')
 
-st.write('Tabela')
+st.write("Tabela")
 
 dataframe = pd.DataFrame({
-  'Nome': ['Josir', 'Bruno', 'Bruna', 'Anna']
-  'Salário: [10, 20, 30, 40]'
-  ])
-  dataframe.style.highlight_max(axis=0)
-  
-  st.write(dataframe)
-  
+    'Nome': ['Josir', 'Bruno', 'Bruna', 'Anna'],
+    'Salário': [10, 20, 30, 40]
+})
+dataframe.style.highlight_max(axis=0)
+
+st.write(dataframe)
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
